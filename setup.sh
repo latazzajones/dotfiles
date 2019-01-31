a1c19741a02e06856546c7ff2495919fb5594a21
Natasha Jones <tasha.k.jones@gmail.com>
2019-01-31 15:50:53 -0500

    Add some color to setup.sh

--- setup.sh
+++ setup.sh
@@ -13,33 +13,38 @@ olddir=~/dotfiles_old
 # list of files/folders to symlink in homedir
 files="bash_profile bashrc gitconfig gitignore_global profile viminfo vim git-completion.bash"    
 # list of packages to run on start
+
+###colors###
+NC='\033[0m'
+BLUE='\033[0;94m'
+ORANGE='\033[0;33m'
+GREEN='\033[0;92m'
+PINK='\033[0;36m'
+MAGENTA='\033[0;35m'
 ##########
 
 # create dotfiles_old in homedir
-echo "Creating $olddir for backup of any existing dotfiles in ~"
+echo "${GREEN}Creating $olddir for backup of any existing dotfiles in ~${NC}"
 mkdir -p $olddir
-echo "...done"
 
 # change to the dotfiles directory
-echo "Changing to the $dir directory"
+echo "${ORANGE}Changing to the $dir directory${NC}"
 cd $dir
-echo "...done"
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
+echo "${BLUE}Moving any existing dotfiles from ~ to $olddir then creating symlink to  in home directory.${NC}"
 for file in $files; do
-    echo "Moving any existing dotfiles from ~ to $olddir"
     mv ~/.$file ~/dotfiles_old/
-    echo "Creating symlink to $file in home directory."
     ln -s $dir/$file ~/.$file
 done
 
 # install submodules
-# will this work? it works but it's adding a new vim file :-( 
-echo "Adding submodules"
+echo "${GREEN}Adding submodules for vim!${NC}" 
 git submodule init
 git submodule update
 
 # remind the user to use the .keys file
-echo "Creating a ~/.keys file"
-touch ~/.keys
-echo "Don't forget to update your ~/.keys file! Have fun!"
+echo "${PINK}Creating a ~/.secret_keys file${NC}"
+touch ~/.secret_keys
+echo "${MAGENTA}Don't forget to update your ~/.keys file!" 
+echo "👩‍💻 👩‍💻 👩‍💻 Have fun!👩‍💻 👩‍💻 👩‍💻"
